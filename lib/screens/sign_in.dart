6401e10e040792f403d11e108b4d73d2836abf00
Muhammad Asif Khan <muhammadasifkhan953@gmail.com>
2023-04-02 02:03:10 -0700

    set up file structure

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:job_posting_app/textfield.dart';
+import 'package:job_posting_app/screens/signup.dart';
 
-import 'Sign_in.dart';
-import 'button.dart';
 
-class SignUp extends StatelessWidget {
-  const SignUp({super.key});
+import '../utils/button.dart';
+import '../utils/textfield.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +18,7 @@ class SignUp extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("Let’s sign you up",
+            const Text("Let’s sign in",
                 style: TextStyle(
                     color: Colors.white,
                     fontSize: 35,
@@ -26,24 +27,25 @@ class SignUp extends StatelessWidget {
             const Text("Welcome\nJoin the community!",
                 style: TextStyle(color: Colors.white, fontSize: 30)),
             const SizedBox(height: 47),
-            const AppTextField(placeholder: "Enter your full name"),
-            const AppTextField(placeholder: "Enter your email address"),
+            const AppTextField(
+              placeholder: "Enter your email address",
+            ),
             const AppTextField(placeholder: "Enter your password"),
             const Spacer(),
             GestureDetector(
               onTap: () {
                 Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const SignIn()));
+                    MaterialPageRoute(builder: (context) => const SignUp()));
               },
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
                   Text(
-                    'Already have an account ? ',
+                    'Don’t have an account ? ',
                     style: TextStyle(color: Color(0xFF8F8F9E), fontSize: 15),
                   ),
                   Text(
-                    'Sign In',
+                    'Register',
                     style: TextStyle(color: Colors.white, fontSize: 15),
                   ),
                 ],
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:job_posting_app/signup.dart';
-import 'package:job_posting_app/textfield.dart';
 
-import 'button.dart';
+import '../Sign_in.dart';
 
-class SignIn extends StatelessWidget {
-  const SignIn({super.key});
+import '../utils/button.dart';
+import '../utils/textfield.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +18,7 @@ class SignIn extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("Let’s sign in",
+            const Text("Let’s sign you up",
                 style: TextStyle(
                     color: Colors.white,
                     fontSize: 35,
@@ -26,25 +27,24 @@ class SignIn extends StatelessWidget {
             const Text("Welcome\nJoin the community!",
                 style: TextStyle(color: Colors.white, fontSize: 30)),
             const SizedBox(height: 47),
-            const AppTextField(
-              placeholder: "Enter your email address",
-            ),
+            const AppTextField(placeholder: "Enter your full name"),
+            const AppTextField(placeholder: "Enter your email address"),
             const AppTextField(placeholder: "Enter your password"),
             const Spacer(),
             GestureDetector(
               onTap: () {
                 Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const SignUp()));
+                    MaterialPageRoute(builder: (context) => const SignIn()));
               },
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
                   Text(
-                    'Don’t have an account ? ',
+                    'Already have an account ? ',
                     style: TextStyle(color: Color(0xFF8F8F9E), fontSize: 15),
                   ),
                   Text(
-                    'Register',
+                    'Sign In',
                     style: TextStyle(color: Colors.white, fontSize: 15),
                   ),
                 ],
